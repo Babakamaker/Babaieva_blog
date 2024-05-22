d859f32e66f766d347078daa2d9ac13cf1ec7c00
Viktoriia Babaieva <viktoriia.babaieva@oa.edu.ua>
2024-05-22 11:01:02 +0300

    Lab 2 Done

--- database/migrations/2024_05_20_215330_add_two_factor_columns_to_users_table.php
+++ database/migrations/2024_05_20_215330_add_two_factor_columns_to_users_table.php
@@ -1,5 +1,4 @@
 <?php
-
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -7,40 +6,46 @@ use Laravel\Fortify\Fortify;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
-        Schema::table('users', function (Blueprint $table) {
-            $table->text('two_factor_secret')
-                ->after('password')
-                ->nullable();
+/**
+* Run the migrations.
+*/
+public function up(): void
+{
+Schema::table('users', function (Blueprint $table) {
+$table->text('two_factor_secret')
+->after('password')
+->nullable();
 
-            $table->text('two_factor_recovery_codes')
-                ->after('two_factor_secret')
-                ->nullable();
+$table->text('two_factor_recovery_codes')
+->after('two_factor_secret')
+->nullable();
 
-            if (Fortify::confirmsTwoFactorAuthentication()) {
-                $table->timestamp('two_factor_confirmed_at')
-                    ->after('two_factor_recovery_codes')
-                    ->nullable();
-            }
-        });
-    }
+if (Fortify::confirmsTwoFactorAuthentication()) {
+$table->timestamp('two_factor_confirmed_at')
+->after('two_factor_recovery_codes')
+->nullable();
+}
+});
+}
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn(array_merge([
-                'two_factor_secret',
-                'two_factor_recovery_codes',
-            ], Fortify::confirmsTwoFactorAuthentication() ? [
-                'two_factor_confirmed_at',
-            ] : []));
-        });
-    }
+/**
+* Reverse the migrations.
+*/
+public function down(): void
+{
+Schema::table('users', function (Blueprint $table) {
+// Dropping columns only if they exist
+if (Schema::hasColumn('users', 'two_factor_secret')) {
+$table->dropColumn('two_factor_secret');
+}
+
+if (Schema::hasColumn('users', 'two_factor_recovery_codes')) {
+$table->dropColumn('two_factor_recovery_codes');
+}
+
+if (Schema::hasColumn('users', 'two_factor_confirmed_at')) {
+$table->dropColumn('two_factor_confirmed_at');
+}
+});
+}
 };
