0910a90ff6796e5a455d60186dbf24095ad21cb7
Viktoriia Babaieva <viktoriia.babaieva@oa.edu.ua>
2024-05-30 13:14:06 +0300

    Lab 13 Done

--- routes/web.php
+++ routes/web.php
@@ -24,8 +24,13 @@ Route::group([ 'namespace' => 'App\Http\Controllers\Blog', 'prefix' => 'blog'], 
 Route::group(['prefix' => 'digging_deeper'], function () {
 
     Route::get('collections', [DiggingDeeperController::class, 'collections'])
-
         ->name('digging_deeper.collections');
+
+    Route::get('process-video', [DiggingDeeperController::class, 'processVideo'])
+        ->name('digging_deeper.processVideo');
+
+    Route::get('prepare-catalog', [DiggingDeeperController::class, 'prepareCatalog'])
+        ->name('digging_deeper.prepareCatalog');
 
 });
 $groupData = [
@@ -42,4 +47,5 @@ Route::group($groupData, function () {
     Route::resource('posts', PostController::class)
         ->except(['show'])                               //не робити маршрут для метода show
         ->names('blog.admin.posts');
+
 });
